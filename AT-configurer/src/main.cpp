ef3c87118ef16fe8621060ee44f4b47bb256e320
Hiro <hiroshi.lestrade@ingenieria.unam.edu>
2022-09-05 23:28:33 -0500

    AT-configurer

---
+++ AT-configurer/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(12,13);
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
